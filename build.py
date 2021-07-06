0ab9b365cf2bb246ed4a6793c870bc323279b1ac
Rebecca Breu <rebecca@rbreu.de>
2021-07-06 20:54:03 +0200

    Run build.yml on all platforms

--- build.py
+++ build.py
@@ -4,8 +4,10 @@
 from distutils.sysconfig import get_python_lib
 import os.path
 import shutil
-import subprocess
 import sys
+
+
+import PyInstaller.__main__
 
 from beeref import constants
 
@@ -15,32 +17,31 @@ def datapath(src, dest):
 
 
 pyqt_dir = os.path.join(get_python_lib(), 'PyQt6', 'Qt6')
-appname= f'{constants.APPNAME}-{constants.VERSION}'
+appname = f'{constants.APPNAME}-{constants.VERSION}'
 
-subprocess.run(
-    [
-        'pyinstaller',
-        '--noconfirm',
-        '--onedir',
-        '--windowed',
-        '--name', appname,
-        '--hidden-import', 'PyQt6.sip',
-        '--hidden-import', 'PyQt6.QtPrintSupport',
-        '--icon', os.path.join('beeref', 'assets', 'logo.png'),
-        '--paths', os.path.join(pyqt_dir, 'bin'),
-        '--add-data', datapath(
-            [pyqt_dir, 'plugins', 'platforms'], ['platforms']),
-        '--add-data', datapath([pyqt_dir, 'lib'], ['.']),
-        '--add-data', datapath(
-            ['beeref', 'documentation'], ['beeref', 'documentation']),
-        '--add-data', datapath(
-            ['beeref', 'assets', '*.png'], ['beeref', 'assets']),
-        'beeref/__main__.py'
-    ])
+PyInstaller.__main__.run([
+    '--noconfirm',
+    '--onedir',
+    '--windowed',
+    '--name', appname,
+    '--hidden-import', 'PyQt6.sip',
+    '--hidden-import', 'PyQt6.QtPrintSupport',
+    '--icon', os.path.join('beeref', 'assets', 'logo.png'),
+    '--paths', os.path.join(pyqt_dir, 'bin'),
+    '--add-data', datapath(
+        [pyqt_dir, 'plugins', 'platforms'], ['platforms']),
+    '--add-data', datapath([pyqt_dir, 'lib'], ['.']),
+    '--add-data', datapath(
+        ['beeref', 'documentation'], ['beeref', 'documentation']),
+    '--add-data', datapath(
+        ['beeref', 'assets', '*.png'], ['beeref', 'assets']),
+    'beeref/__main__.py'
+])
 
-print('Zipping output...')
+outfilename = os.path.join('dist', f'{appname}-{sys.platform}')
+print(f'Zipping output to {outfilename}.zip')
 shutil.make_archive(
-    os.path.join('dist', appname),
+    outfilename,
     'zip',
     root_dir='dist',
     base_dir=appname)
